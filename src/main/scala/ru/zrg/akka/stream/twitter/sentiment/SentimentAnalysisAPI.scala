df24312ea76e27dd2995b7ec4a8fc94fec295ed5
Roman Zarubin <rgzarubin.sbt@sberbank.ru>
2016-10-28 22:28:44 +0300

    add object SentimentType with pattern matching

--- src/main/scala/ru/zrg/akka/stream/twitter/sentiment/SentimentAnalysisAPI.scala
+++ src/main/scala/ru/zrg/akka/stream/twitter/sentiment/SentimentAnalysisAPI.scala
@@ -13,6 +13,19 @@ trait SentimentType {
   override def toString = name
 }
 
+object SentimentType {
+
+  def apply(sentimentId: Int) = sentimentId match {
+    case 0 => VERY_NEGATIVE
+    case 1 => NEGATIVE
+    case 2 => NEUTRAL
+    case 3 => POSITIVE
+    case 4 => VERY_POSITIVE
+    case _ => NEUTRAL
+  }
+
+}
+
 case object VERY_NEGATIVE extends SentimentType { val name = "very negative"}
 case object NEGATIVE extends SentimentType { val name = "negative"}
 case object NEUTRAL extends SentimentType { val name = "neutral"}
@@ -41,13 +54,6 @@ object SentimentAnalysisAPI {
 
     val resultSentiment = math.ceil(sentiments.foldLeft(0.0) {_ + _} / sentiments.length).toInt
 
-    resultSentiment match {
-      case 0 => VERY_NEGATIVE
-      case 1 => NEGATIVE
-      case 2 => NEUTRAL
-      case 3 => POSITIVE
-      case 4 => VERY_POSITIVE
-      case _ => NEUTRAL
-    }
+    SentimentType(resultSentiment)
   }
 }
